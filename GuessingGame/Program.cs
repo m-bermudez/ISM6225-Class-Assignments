8d2db23fb250343a1019ff29ec888b7d48b58e1b
Maui Bermudez <maur.berm@gmail.com>
2025-03-04 11:58:55 -0500

    Made a guessing game

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace GuessingGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Guess a number between 1 and 20");
+        Random rnd = new Random();
+        int answer = rnd.Next(1, 20);
+        int guess = 0;
+
+        while (guess != answer){
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess > answer){
+                Console.WriteLine("Too high!");
+            }
+            else if (guess < answer){
+                Console.WriteLine("Too low!");
+            }
+            else {
+                Console.WriteLine($"Congrats! The answer was {answer}");
+            } 
+        }
+    }
+}
