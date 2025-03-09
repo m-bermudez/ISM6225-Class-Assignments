aed422837dd398b542511f52df718361d4e96161
Maui B <maur.berm@gmail.com>
2025-03-09 16:44:27 -0400

    Added Kattis workspace

---
+++ Kattis Problems/KattisApps/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace KattisApps;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> modulos = new HashSet<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            int nums = int.Parse(Console.ReadLine());
+            int modulo = nums % 42;
+            modulos.Add(modulo);
+        }
+        Console.WriteLine(modulos.Count());
+    }
+}
